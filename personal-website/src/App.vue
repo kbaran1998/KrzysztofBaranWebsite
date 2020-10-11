aedb65b10bcc571c4399e4c6c574b10e7c983a36
Krzysztof <krysbaran@gmail.com>
2020-10-11 18:11:34 +0200

    Add bootstrap navbar

--- personal-website/src/App.vue
+++ personal-website/src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <Main />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Main from './components/MainView';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Main,
   },
 };
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ personal-website/src/components/TheNavBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-navbar
+    toggleable="lg"
+    type="dark"
+    variant="info"
+    class="navbar-horizontal"
+    id="sideNav"
+  >
+    <b-navbar-brand href="#">Krzysztof Baran</b-navbar-brand>
+  </b-navbar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+@media (min-width: 992px) {
+  #sideNav {
+    text-align: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: -webkit-box !important;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    width: 17rem;
+    height: 100vh;
+  }
+  #header h1 {
+    margin: 0px;
+  }
+}
+</style>
